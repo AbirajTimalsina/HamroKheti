1315bf8f9afe4f0e64307fe240ea79f857f2058b
AbirajTimalsina <timalsinaabiraj92@gmail.com>
2020-09-20 14:22:30 +0545

    validation completed !!

--- app/src/main/java/com/example/hamrokheti/WheatherActivity.java
+++ app/src/main/java/com/example/hamrokheti/WheatherActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -50,6 +51,11 @@ public class WheatherActivity extends AppCompatActivity {
         imgSrc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+               if (TextUtils.isEmpty(etSrc.getText())){
+                   etSrc.setError("मौसम जानकारी हेर्न कृपया तपाईको शहरको नाम टाइप गर्नुहोस्");
+                   etSrc.requestFocus();
+                   return;
+               }
                 getWeatherData(etSrc.getText().toString().trim());
             }
         });
